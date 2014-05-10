67f7bc2ced6aecc2ba71f1da2fde050912076167
alexisprunier <prunier.al@gmail.com>
2014-05-10 14:15:51 +0200

    Last version of SH

--- SportsHubApp/views.py
+++ SportsHubApp/views.py
@@ -27,9 +27,10 @@ def refresh(request):
     for _, src in filters.items():
         sources += src.split(";")
     sources.append(None)
+    sport_filter = filters["Sports"].split(";") if "Sports" in filters.keys() else []
     nb_new_article = Article.objects.filter(id__gt=int(request.POST.get('last_id', False)),
                                           type__in=filters.keys(),
-                                          sport__in=filters["Sports"].split(";"),
+                                          sport__in=sport_filter,
                                           origine__in=sources).count()
     sended_text = str(nb_new_article) + " Nouvelles Actus !"
     if nb_new_article != 0:
@@ -45,47 +46,13 @@ def get_news(request):
     for _, src in filters.items():
         sources += src.split(";")
     sources.append(None)
+    sport_filter = filters["Sports"].split(";") if "Sports" in filters.keys() else []
     new_articles = Article.objects.filter(id__gt=int(request.POST.get('last_id', False)),
                                           type__in=filters.keys(),
-                                          sport__in=filters["Sports"].split(";"),
+                                          sport__in=sport_filter,
                                           origine__in=sources)
     new_articles = reversed(new_articles)
-    content = ''
-    print new_articles
-    for article in new_articles:
-        if article.type == "Articles":
-            content += "\
-                <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
-                    <div class='title-new'>" + unicode(article.titre) + "</div>\
-                    <div class='origine-new'><div class='origine-name'>" + unicode(article.origine) + "</div><img class='favicon' src='" + unicode(request.POST.get('static_url', False)) + "favicon/" + unicode(article.origine) + ".png'/></div>\
-                    <div class='date-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
-                    <div class='bottom-bar " + unicode(article.sport)+ "'></div>\
-                    <img class='img-new' src='" + unicode(article.image) + "'/>\
-                </div>"
-        
-        if article.type == "Twitter":
-            content += "\
-                <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
-                    <div class='title'>" + unicode(article.titre) +"</div>\
-                    <div class='origine'>" + unicode(article.origine) + "</div>\
-                    <img class='twitter-img' src='" + unicode(request.POST.get('static_url', False)) + "twitter.png'/>\
-                    <img class='open-quote' src='" + unicode(request.POST.get('static_url', False)) + "open-quote.png'/>\
-                    <img class='close-quote' src='" + unicode(request.POST.get('static_url', False)) + "close-quote.png'/>\
-                    <div class='date-tw-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
-                    <div class='bottom-tw-bar " + unicode(article.sport)+ "'></div>\
-                </div>"
-        
-        if article.type == "Facebook":
-            content += "\
-                <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
-                    <div class='title'>" + unicode(article.titre) +"</div>\
-                    <div class='origine'>" + unicode(article.origine) + "</div>\
-                    <img class='facebook-img' src='" + unicode(request.POST.get('static_url', False)) + "facebook.png'/>\
-                    <img class='open-quote' src='" + unicode(request.POST.get('static_url', False)) + "open-quote.png'/>\
-                    <img class='close-quote' src='" + unicode(request.POST.get('static_url', False)) + "close-quote.png'/>\
-                    <div class='date-fb-new " + unicode(article.sport)+ "'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
-                    <div class='bottom-fb-bar'></div>\
-                </div>"
+    content = get_html(new_articles, request);
     
     return HttpResponse(content)
 
@@ -97,45 +64,74 @@ def apply_news(request):
     for _, src in filters.items():
         sources += src.split(";")
     sources.append(None)
-    new_articles = Article.objects.filter(type__in=filters.keys(),
-                                          sport__in=filters["Sports"].split(";"),
-                                          origine__in=sources)
+    
+    sport_filter = filters["Sports"].split(";") if "Sports" in filters.keys() else []
+    number_articles = Article.objects.filter(type__in=filters.keys(),
+                                             sport__in=sport_filter,
+                                             origine__in=sources).count()
+    if number_articles >= 1:
+        if number_articles < 30:
+            quantity = number_articles
+        else:
+            quantity = 30
+        sport_filter = filters["Sports"].split(";") if "Sports" in filters.keys() else []
+        new_articles = Article.objects.filter(type__in=filters.keys(),
+                                          sport__in=sport_filter,
+                                          origine__in=sources)[number_articles-quantity:]
+    else:
+        new_articles = []
+    
     new_articles = reversed(new_articles)
-    content = ''
+    
     print new_articles
+    content = get_html(new_articles, request);
+
+    return HttpResponse(content)
+
+def get_html(new_articles, request):
+    content = ''
     for article in new_articles:
         if article.type == "Articles":
+            if article.image != "":
+                image = "<img class='img-new' src='" + unicode(article.image) + "'/>"
+            else:
+                image = "<img class='img-new' src='" + unicode(request.POST.get('static_url', False)) + "/no-image.png'/>"
             content += "\
-                <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
-                    <div class='title-new'>" + unicode(article.titre) + "</div>\
-                    <div class='origine-new'><div class='origine-name'>" + unicode(article.origine) + "</div><img class='favicon' src='" + unicode(request.POST.get('static_url', False)) + "favicon/" + unicode(article.origine) + ".png'/></div>\
-                    <div class='date-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
-                    <div class='bottom-bar " + unicode(article.sport)+ "'></div>\
-                    <img class='img-new' src='" + unicode(article.image) + "'/>\
-                </div>"
+                <a href='" + unicode(article.lien) + "' target='_blank'>\
+                    <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
+                        <div class='title-new'>" + unicode(article.titre) + "</div>\
+                        <div class='origine-new'><div class='origine-name'>" + unicode(article.origine) + "</div><img class='favicon' src=\"" + unicode(request.POST.get('static_url', False)) + "favicon/" + unicode(article.origine) + ".png\"/></div>\
+                        <div class='date-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
+                        <div class='bottom-bar " + unicode(article.sport)+ "'></div>\
+                        " + image + "\
+                    </div>\
+                </a>"
         
         if article.type == "Twitter":
             content += "\
-                <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
-                    <div class='title'>" + unicode(article.titre) +"</div>\
-                    <div class='origine'>" + unicode(article.origine) + "</div>\
-                    <img class='twitter-img' src='" + unicode(request.POST.get('static_url', False)) + "twitter.png'/>\
-                    <img class='open-quote' src='" + unicode(request.POST.get('static_url', False)) + "open-quote.png'/>\
-                    <img class='close-quote' src='" + unicode(request.POST.get('static_url', False)) + "close-quote.png'/>\
-                    <div class='date-tw-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
-                    <div class='bottom-tw-bar " + unicode(article.sport)+ "'></div>\
-                </div>"
+                <a href='" + unicode(article.lien) + "' target='_blank'>\
+                    <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
+                        <div class='title'>" + unicode(article.titre) +"</div>\
+                        <div class='origine'>" + unicode(article.origine) + "</div>\
+                        <img class='twitter-img' src='" + unicode(request.POST.get('static_url', False)) + "twitter.png'/>\
+                        <img class='open-quote' src='" + unicode(request.POST.get('static_url', False)) + "open-quote.png'/>\
+                        <img class='close-quote' src='" + unicode(request.POST.get('static_url', False)) + "close-quote.png'/>\
+                        <div class='date-tw-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
+                        <div class='bottom-tw-bar " + unicode(article.sport)+ "'></div>\
+                    </div>\
+                </a>"
         
         if article.type == "Facebook":
             content += "\
-                <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
-                    <div class='title'>" + unicode(article.titre) +"</div>\
-                    <div class='origine'>" + unicode(article.origine) + "</div>\
-                    <img class='facebook-img' src='" + unicode(request.POST.get('static_url', False)) + "facebook.png'/>\
-                    <img class='open-quote' src='" + unicode(request.POST.get('static_url', False)) + "open-quote.png'/>\
-                    <img class='close-quote' src='" + unicode(request.POST.get('static_url', False)) + "close-quote.png'/>\
-                    <div class='date-fb-new'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
-                    <div class='bottom-fb-bar " + unicode(article.sport)+ "'></div>\
-                </div>"
-    
-    return HttpResponse(content)
+                <a href='" + unicode(article.lien) + "' target='_blank'>\
+                    <div class='new' id='" + unicode(article.id) + "' style='display: none'>\
+                        <div class='title'>" + unicode(article.titre) +"</div>\
+                        <div class='origine'>" + unicode(article.origine) + "</div>\
+                        <img class='facebook-img' src='" + unicode(request.POST.get('static_url', False)) + "facebook.png'/>\
+                        <img class='open-quote' src='" + unicode(request.POST.get('static_url', False)) + "open-quote.png'/>\
+                        <img class='close-quote' src='" + unicode(request.POST.get('static_url', False)) + "close-quote.png'/>\
+                        <div class='date-fb-new " + unicode(article.sport)+ "'>" + unicode(article.date.strftime('%H:%M')) + "</div>\
+                        <div class='bottom-fb-bar'></div>\
+                    </div>\
+                </a>"
+    return content
--- SportsHubScript/main.py
+++ SportsHubScript/main.py
@@ -6,10 +6,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings-sh")
 
 from SportsHubScript.Twitter import Twitter
 from SportsHubScript.Facebook import Facebook
-from Articles.lequipe import Lequipe
-from Articles.sports import Sports
-from Articles.yahoo import YahooSport
-from Articles.eurosport import Eurosport
+from Articles.ArticleGetter import Eurosport 
+from Articles.ArticleGetter import Sports 
+from Articles.ArticleGetter import YahooSport 
+from Articles.ArticleGetter import Sport365 
+from Articles.ArticleGetter import Lequipe 
+from Articles.ArticleGetter import BasketUSA
+from Articles.ArticleGetter import BasketSession
+from Articles.ArticleGetter import FranceFootball
+from Articles.ArticleGetter import FootMercato
 
 GLOBAL_nb_infos = 3
 
@@ -19,16 +24,21 @@ lequipe = Lequipe(GLOBAL_nb_infos)
 sports = Sports(GLOBAL_nb_infos)
 yahoo = YahooSport(GLOBAL_nb_infos)
 eurosport = Eurosport(GLOBAL_nb_infos)
+sport365 = Sport365(GLOBAL_nb_infos)
+franceFootball = FranceFootball(GLOBAL_nb_infos)
+footMercato = FootMercato(GLOBAL_nb_infos)
+basketUsa = BasketUSA(GLOBAL_nb_infos)
+basketSession = BasketSession(GLOBAL_nb_infos)
 
 #FB
 print "DEBUT Facebook"
 sources = {"beIN SPORTS France": 301300799953924,
-           "Intersport": 10151773703860786,
-           "Decathlon": 165823830131654,
-           "Sport2000": 221584158041000,
-           "Equipe de France de Football": 186982054657561,
-           "Equipe de France de Basketball": 10151630582241174,
-           "Equipe de France de Handball": 113556715321615}
+           "Intersport": 43767900785,
+           "Decathlon": 122695097770591,
+           "Sport2000": 122191497979866,
+           "Equipe de France de Football": 112215632152510,
+           "Equipe de France de Basketball": 123564846173,
+           "Equipe de France de Handball": 69895790042}
 
 for source, ident in sources.items() :
     print "Facebook: "+source
@@ -36,13 +46,13 @@ for source, ident in sources.items() :
 
 #TWITTER
 print "DEBUT Twitter"
-sources = ["Gael_Monfils",
-           "airlavillenie",
-           "julienbenneteau",
-           "nicolas88batum",
-           "MATUIDIBlaise",
-           "JrmeFernandez",
-           "sebchabal",
-           "NKARABATIC"]
+sources = ["Gael Monfils",
+           "Renaud Lavillenie",
+           "julien benneteau",
+           "Nicolas Batum",
+           "Blaise MATUIDI",
+           "Jérôme Fernandez",
+           "Sébastien Chabal",
+           "NIKOLA KARABATIC",]
 o_twitter = Twitter(sources, GLOBAL_nb_infos)
     
